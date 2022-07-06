a1e7091373d6923164a10b73b9ecb1c661efd9ac
dinakartejat <dinakarteja.tanniru@prasthana.com>
2022-07-06 10:43:57 +0530

    first commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_30_101913) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_06_051322) do
   create_table "acs", force: :cascade do |t|
     t.string "name"
     t.integer "price"
@@ -78,6 +78,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_30_101913) do
   create_table "shapes", force: :cascade do |t|
     t.string "name"
     t.string "colour"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "stories", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
